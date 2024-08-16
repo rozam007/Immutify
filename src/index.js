affab173fd0e8feadcfa3c97a0dd3460d563f00d
rozam007 <rozam0040@gmail.com>
2024-08-16 21:29:31 +0500

    create index.js including main validation logic

--- src/index.js
+++ src/index.js
@@ -1,59 +1,29 @@
+class Validate {
+  constructor(target, validators) {
+    this.target = target;
+    this.validators = validators;
 
-let target = [
-  {
-    name: "rozam",
-    city: "gujranwala",
-    age: 24,
-  },
-  {
-    name: "afaq",
-    city: "gujranwala",
-    age: 22,
-  },
-  {
-    name: "adil",
-    city: "gujranwala",
-    age: 27,
-  },
-];
+    return new Proxy(this.target, this.createHandler());
+  }
 
-target = target.map(person => new Proxy(person, {
-    get: (target, prop) => {
-        if(prop in target){
-            if(prop === 'age'){
-                if(target[prop] >= 0 && target[prop] <= 24){
-                    return target[prop]
-                }
-                else{
-                    return new RangeError('Age is too high or too low')
-                }
-            }
+  createHandler() {
+    return {
+      set: (target, key, value) => {
+        if (this.validators[key]) {
+          const isValid = this.validators[key](value);
+          if (!isValid) {
+            throw new TypeError(`Validation failed for key "${key}" with value "${value}".`);
+          }
+        } else {
+          throw new ReferenceError(`No validator defined for key "${key}".`);
         }
-        else{
-            throw new TypeError('Property not found!!')
-        }
-    },
-    set: (target, prop, receiver) => {
-        if(prop in target){
-            if(prop === 'age'){
-                if(receiver > 30){
-                    throw new RangeError('Age is too high')
-                }
-                if(receiver < 18){
-                    throw new RangeError('Age is too low')
-                }
-            }
-        }
-    }
-}))
 
-target.forEach(person => {
-    try {
-        console.log('get age: ', person['age'])
-    } catch (error) {
-        console.log(error['name', error['message']])
-    }
-})
+        target[key] = value;
+        console.log(`Value for key "${key}" has been updated to ${value}.`);
+        return true;
+      },
+    };
+  }
+}
 
-console.log('set age: ', target[0]['age'] = 50)
-
+module.exports = { Validate };
