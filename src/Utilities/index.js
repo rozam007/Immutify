03cc87c5b0ac6275b474d25b5b1fd33ac46e58ce
rozam007 <rozam0040@gmail.com>
2024-08-22 20:10:28 +0500

    make a utility function to match just keys between target object and validator object

---
+++ src/Utilities/index.js
@@ -0,0 +1,13 @@
+const matchObjectsKeys = (obj1, obj2) => {
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = OBject.keys(obj2);
+
+    for(const key of obj1Keys){
+        if(!obj2Keys.includes(key)){
+            throw new Error("keys mistached between target object and validator object!!");
+        }
+    }
+    
+}
+
+module.exports = {matchObjectsKeys}
