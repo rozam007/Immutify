8742578abf8cab335fc84f142a0375caff5888f0
rozam007 <rozam0040@gmail.com>
2024-08-24 13:42:27 +0500

    mention mismatched key in the error string

--- src/Utilities/index.js
+++ src/Utilities/index.js
@@ -1,10 +1,10 @@
 const matchObjectsKeys = (obj1, obj2) => {
     const obj1Keys = Object.keys(obj1);
-    const obj2Keys = OBject.keys(obj2);
+    const obj2Keys = Object.keys(obj2);
 
     for(const key of obj1Keys){
         if(!obj2Keys.includes(key)){
-            throw new Error("keys mistached between target object and validator object!!");
+            throw new Error(`mismatched key: ${key}`);
         }
     }
     
