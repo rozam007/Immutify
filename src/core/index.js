ac1b520aac55ec0a50ef2ee831bc29773942cf94
rozam007 <rozam0040@gmail.com>
2024-09-06 20:16:42 +0500

    match type constructor for both cases

--- src/core/index.js
+++ src/core/index.js
@@ -1,16 +1,19 @@
 const {Validator} = require('./Validator');
 const {StringValidator} = require('./StringValidator');
 const {NumberValidator} = require('./NumberValidator');
+const { emailValidator } = require('./EmailValidator');
 
 const type = (constructor) => {
-  if (constructor === String) {
+  if (constructor === 'String' || constructor === 'string') {
     return new StringValidator(constructor);
-  } else if (constructor === Number) {
+  } else if (constructor === 'Number' || constructor === 'number' ) {
     return new NumberValidator(constructor);
-  } else if (constructor === Boolean) {
-    return new Validator(constructor);
+  } else if ( constructor === 'Email' || constructor === 'email' ) {
+    return emailValidator(constructor);
+  } else if (constructor === 'Boolean' === constructor === 'boolean') {
+    return new Validator(constructor)
   }
-  throw new Error("Unsupported type");
+  // throw new Error("Unsupported type");
 };
 
 module.exports = { type };
